053edbc155c72f9bfec9140cf3407028fd1f5885
Adrian <aabeijoncarbajo@danielcastelao.org>
2024-04-05 10:05:07 +0200

    Renombro prueboCalculadora a prueboMultiplicacion y añado negativas

--- Tests/OperacionesTests.java
+++ Tests/OperacionesTests.java
@@ -10,15 +10,32 @@ public class OperacionesTests {
        Assertions.assertEquals(10f, Operaciones.operacionBasica(2f,5f,MULTIPLICACION));
     }
     @Test
+    public void prueboMultiplicacionNegativa(){
+        Assertions.assertEquals(-10f, Operaciones.operacionBasica(-2f,5f,MULTIPLICACION));
+    }
+    @Test
     public void prueboDivision(){
         Assertions.assertEquals(5f, Operaciones.operacionBasica(50f,10f,DIVISION));
     }
+    @Test
+    public void prueboDivisionNegativa(){
+        Assertions.assertEquals(-5f, Operaciones.operacionBasica(-50f,10f,DIVISION));
+    }
+
     @Test
     public void prueboSuma(){
         Assertions.assertEquals(30f, Operaciones.operacionBasica(10f, 20f, SUMA));
     }
     @Test
+    public void prueboSumaNegativa(){
+        Assertions.assertEquals(10f, Operaciones.operacionBasica(-10f, 20f, SUMA));
+    }
+    @Test
     public void prueboResta(){
         Assertions.assertEquals(7f, Operaciones.operacionBasica(20f, 13f, RESTA));
+    }
+    @Test
+    public void prueboRestaNegativa(){
+        Assertions.assertEquals(-33f, Operaciones.operacionBasica(-20f, 13f, RESTA));
     }
 }
