d2caa2b668155f8a71506f83bf06a73aaeabf602
Adrian <aabeijoncarbajo@danielcastelao.org>
2024-04-05 10:06:40 +0200

    Renombro prueboCalculadora a prueboMultiplicacion y añado negativas y cero

--- Tests/OperacionesTests.java
+++ Tests/OperacionesTests.java
@@ -14,6 +14,10 @@ public class OperacionesTests {
         Assertions.assertEquals(-10f, Operaciones.operacionBasica(-2f,5f,MULTIPLICACION));
     }
     @Test
+    public void prueboMultiplicacionConCero(){
+        Assertions.assertEquals(0f, Operaciones.operacionBasica(0f,5f,MULTIPLICACION));
+    }
+    @Test
     public void prueboDivision(){
         Assertions.assertEquals(5f, Operaciones.operacionBasica(50f,10f,DIVISION));
     }
